52ce092ca71217d7c667ff3d56817558d5238644
Daniel321W <162799995+Daniel321W@users.noreply.github.com>
2026-02-22 00:58:10 +0100

    Update 1st page

---
+++ src/data/products.js
@@ -0,0 +1,40 @@
+export const products = [
+  {
+    id: 1,
+    title: "Froggie Cat",
+    price: "25 PLN",
+    image: "/images/Froggie Cat.jpeg",
+    category: "Zwierzęta",
+    vintedLink: "https://www.vinted.pl/member/twoja-nazwa",
+  },
+  {
+    id: 2,
+    title: "Wydra W Akwarium",
+    price: "15 PLN",
+    image: "/images/Wydra W Akwarium.jpeg",
+    category: "Zwierzęta",
+    vintedLink: "https://www.vinted.pl/",
+  },
+  {
+    id: 3,
+    title: "Kotek Marie",
+    price: "30 PLN",
+    image: "/images/Kotek Marie.jpeg",
+    category: "Zwierzęta",
+    vintedLink: "https://www.vinted.pl/",
+  },
+  {
+    id: 4,
+    title: "Bloom Winx Club",
+    price: "20 PLN",
+    image: "/images/Bloom Winx Club.jpeg",
+    category: "Bajki",
+    vintedLink: "https://www.vinted.pl/",
+  },
+  
+
+
+
+
+
+];
